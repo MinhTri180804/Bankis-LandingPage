c80f61d51c01f355fd8192274c983c2d5ffa9632
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-03-20 11:20:44 +0700

    init(source): init source structure
    This commit is inti source structure for project
    
    Co-authored-by: Nguyen Minh Tri <nguyenminhtri1808t@gmail.com>

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+
+const logo = document.getElementById('logo');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.style.width = '120%';
+
+message.innerHTML =
+  'We use cookied for imporved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
