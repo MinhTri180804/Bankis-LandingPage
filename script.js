4d6ac96db8d9c49f31b429f6b0b28ed0e8e01195
Minh Tri <nguyenminhtri1808t@gmail.com>
2025-03-20 14:35:43 +0700

    feat(scrollToSection): implement feature scroll to section nav
    This commit is implement feature scroll to section page based customer
    select nav element
    
    Co-authored-by: Minh Tri <nguyenminhtri1808t@gmail.com>

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+const navLinkElements = document.querySelectorAll('a.nav__link');
+const section_1 = document.querySelector('#section--1');
+const sectionElements = document.querySelectorAll('section.section');
 
 const logo = document.getElementById('logo');
 const message = document.createElement('div');
@@ -36,5 +39,30 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+window.addEventListener('load', () => {
+  if (window.location.hash) {
+    setTimeout(() => {
+      const locationHash = window.location.hash;
+      document
+        .querySelector(locationHash)
+        .scrollIntoView({ behavior: 'smooth' });
+    }, 100);
+  } else {
+    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+  }
+});
+
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+  const targetElement = e.target;
+  if (e.target.classList.contains('nav__link')) {
+    const idElementScrollTo = targetElement.getAttribute('href');
+    history.pushState(null, null, idElementScrollTo);
+    document
+      .querySelector(idElementScrollTo)
+      .scrollIntoView({ behavior: 'smooth' });
   }
 });
